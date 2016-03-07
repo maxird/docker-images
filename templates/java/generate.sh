4bc22a296c4713e0f944f85b3c09301e0c737373
ryan <ryanvaniderstine@maximus.com>
2016-03-07 14:44:08 -0400

    Adding Oracle java images with policy files

---
+++ templates/java/generate.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+SRCDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+HEREDIR=`basename $SRCDIR`
+OUTDIR="$SRCDIR/../../images/$HEREDIR"
+
+function process
+{
+  image=$1
+  base=$2
+  version=$3
+  outpath="$OUTDIR/$image/$base/$version"
+  mkdir -p "$outpath"
+  sed \
+      -e "s|BASE_IMAGE|$image|" \
+      -e "s|BASE_FROM_VERSION|$base|" \
+      -e "s|APP_VERSION|$version|" \
+      Dockerfile > "$outpath/Dockerfile"
+}
+
+BASE_IMAGE="centos"
+
+BASES="6 7"
+
+VERSIONS=" \
+  8u74b02
+"
+
+for b in $BASES; do
+  for v in $VERSIONS; do
+    process $BASE_IMAGE $b $v
+  done
+done
