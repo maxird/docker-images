a97292d4522257a27f781486adb406dfd56eed80
ryan <ryanvaniderstine@maximus.com>
2016-03-07 17:48:13 -0400

    Adding template and generated images for cordra

---
+++ templates/cordra/generate.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+SRCDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+HEREDIR=`basename $SRCDIR`
+OUTDIR="$SRCDIR/../../images/$HEREDIR"
+
+function process
+{
+  image=$1
+  base=$2
+  version=$3
+  outpath="$OUTDIR/$image/$base/$version"
+  mkdir -p "$outpath"
+  sed \
+      -e "s|BASE_IMAGE|$image|" \
+      -e "s|BASE_FROM_VERSION|$base|" \
+      -e "s|APP_VERSION|$version|" \
+      Dockerfile > "$outpath/Dockerfile"
+}
+
+BASE_IMAGE="java"
+
+BASES="6-8 7-8"
+
+VERSIONS=" \
+  1.0.7 \
+"
+
+for b in $BASES; do
+  for v in $VERSIONS; do
+    process $BASE_IMAGE $b $v
+  done
+done
