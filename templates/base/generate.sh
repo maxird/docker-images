bfbba66b8e3de8bbbd65aca8edca65ec99974b80
ryan <ryanvaniderstine@maximus.com>
2016-03-07 12:33:33 -0400

    First pass at generating multiple images

---
+++ templates/base/generate.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+SRCDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+HEREDIR=`basename $SRCDIR`
+OUTDIR="$SRCDIR/../../images/$HEREDIR"
+
+function process
+{
+  version=$1
+  outpath="$OUTDIR/$version"
+  mkdir -p "$outpath"
+  sed -e "s|BASE_FROM_VERSION|$version|" Dockerfile > "$outpath/Dockerfile"
+}
+
+process 6.7
+process 7.2.1511
