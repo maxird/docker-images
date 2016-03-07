64179a81a73542f057a62da6829a37a12dc89fcc
ryan <ryanvaniderstine@maximus.com>
2016-03-07 13:05:31 -0400

    Adding node variants

---
+++ templates/node/generate.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+SRCDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+HEREDIR=`basename $SRCDIR`
+OUTDIR="$SRCDIR/../../images/$HEREDIR"
+
+function process
+{
+  image=$1
+  base=$2
+  version=$3
+  outpath="$OUTDIR/$image/$base/$version"
+  mkdir -p "$outpath"
+  sed \
+      -e "s|BASE_IMAGE|$image|" \
+      -e "s|BASE_FROM_VERSION|$base|" \
+      -e "s|APP_VERSION|$version|" \
+      Dockerfile > "$outpath/Dockerfile"
+}
+
+BASE_IMAGE="centos"
+
+BASES="6 7"
+
+VERSIONS=" \
+  4.3.2
+  5.7.1
+"
+
+for b in $BASES; do
+  for v in $VERSIONS; do
+    process $BASE_IMAGE $b $v
+  done
+done
