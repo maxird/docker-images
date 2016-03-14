d9fc448f43f82f0727ca9c1b6b3cd751a67c4694
ryan <ryanvaniderstine@maximus.com>
2016-03-14 14:47:37 -0300

    Adding two consul-based application proxy/balancer patterns

---
+++ templates/lbapp/generate.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+SRCDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+HEREDIR=`basename $SRCDIR`
+OUTDIR="$SRCDIR/../../images/$HEREDIR"
+
+function process
+{
+  image=$1
+  base=$2
+  version=$3
+  outpath="$OUTDIR/$image/$base"
+  mkdir -p "$outpath"
+  sed \
+      -e "s|BASE_IMAGE|$image|" \
+      -e "s|BASE_FROM_VERSION|$base|" \
+      -e "s|APP_VERSION|$version|" \
+      Dockerfile > "$outpath/Dockerfile"
+  cp $SRCDIR/monitor.sh $outpath/monitor.sh
+  cp $SRCDIR/reload.sh $outpath/reload.sh
+  cp $SRCDIR/template.ctmpl $outpath/template.ctmpl
+}
+
+BASE_IMAGE="centos"
+
+BASES="7"
+
+for b in $BASES; do
+  process $BASE_IMAGE $b $NODE_VERSION
+done
---
+++ templates/lbcolor/monitor.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+if [ -z "$CONSUL" ]; then
+  export CONSUL=consul:8500
+fi
+
+if [ -n "$PROXY_TAG" ]; then
+  export PROXY_TAG="${PROXY_TAG}."
+fi
+if [ -n "$PROXY_DC" ]; then
+  export PROXY_DC="@${PROXY_DC}"
+fi
+if [ -z "$PROXY_PORT" ]; then
+  export PROXY_PORT=80
+fi
+if [ -z "$PROXY_TAG" ]; then
+  export PROXY_TAG=
+fi
+if [ -z "$PROXY_DC" ]; then
+  export PROXY_DC=
+fi
+if [ -z "$PROXY_SERVICE" ]; then
+  export PROXY_SERVICE=
+fi
+if [ -z "$PROXY_PASSWORD" ]; then
+  export PROXY_PASSWORD=`uuidgen`
+fi
+
+echo "---------------------------------------------------------------------------"
+echo "CONSUL:               [$CONSUL]"
+echo "PROXY_COLOR_PATH:     [$PROXY_COLOR_PATH]"
+echo "PROXY_PORT:           [$PROXY_PORT]"
+echo "PROXY_SERVICE:        [$PROXY_SERVICE]"
+echo "PROXY_DC:             [$PROXY_DC]"
+echo "PROXY_TAG:            [$PROXY_TAG]"
+echo "PROXY_PASSWORD:       [$PROXY_PASSWORD]"
+echo "---------------------------------------------------------------------------"
+
+echo "validating environment settings"
+retcode=0
+if [ -z "$PROXY_COLOR_PATH" ]; then
+  echo "error: require PROXY_COLOR_PATH consul path to operate"
+  retcode=1
+fi
+if [ -z "$PROXY_SERVICE" ]; then
+  echo "error: require PROXY_SERVICE to know what service to proxy"
+  retcode=1
+fi
+
+if [ $retcode == 1 ]; then
+  exit $retcode
+fi
+
+echo "altering template to match proxy environment"
+# transfer PROXY_ and SERVICE_ entries to string replacements
+#
+rm -f /proxy/commands.sed
+for i in `env | grep PROXY_`; do
+  echo $i | sed 's|=| |' | awk '{print "s|" $1 "|" $2 "|g" }' >> commands.sed
+done
+for i in `env | grep SERVICE_`; do
+  echo $i | sed 's|=| |' | awk '{print "s|" $1 "|" $2 "|g" }' >> commands.sed
+done
+
+sed -i -f /proxy/commands.sed /proxy/template.ctmpl
+
+echo "---------------------------------------------------------------------------"
+cat /proxy/template.ctmpl
+echo "---------------------------------------------------------------------------"
+
+echo "starting consul monitoring"
+/usr/local/bin/consul-template \
+    -consul $CONSUL \
+    -wait "1s:10s" \
+    -template template.ctmpl:/etc/haproxy/haproxy.cfg:/proxy/reload.sh
+
+retcode=$?
+
+if [ $retcode != 12 ]; then
+  echo "Should only be here if process requested exit: $?"
+fi
+
+exit $retcode
