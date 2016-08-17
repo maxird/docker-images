65a3f2686557400c2a58c99ac761c63b26df6536
ryan <ryanvaniderstine@maximus.com>
2016-08-17 10:47:49 -0300

    Updates to the node packages

---
+++ templates/node-packaging/nsp-check-deep.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+## rewrite a package.nsp.json with explicit versions
+#
+out=package.nsp.json
+cp package.json $out
+
+echo "scanning node_modules folder for packages"
+cd node_modules
+for module in $(/bin/ls -1 -d *); do
+  version=$(cat $module/package.json | jq -r .version)
+  # echo "$module -> $version"
+  cat ../$out | jq ".dependencies[\"$module\"] = \"$version\"" > "../$out.tmp"
+  mv "../$out.tmp" ../$out
+done
+cd ..
+
+## temporarily replace package.json with package.nsp.json
+## and run nsp against it
+#
+echo "running nsp"
+cp package.json package.nsp-safe.json
+mv $out package.json
+nsp check
+result=$?
+mv package.json $out
+if [ $result -ne 0 ]; then
+  echo "package with issues saved as $out"
+else
+  echo "clean package saved as $out"
+fi
+mv package.nsp-safe.json package.json
+
+exit $result
+
