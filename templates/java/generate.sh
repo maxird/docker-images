7eb852921cc8aec95df4f01d5ef781ddde839c57
ryan <ryanvaniderstine@maximus.com>
2017-02-01 10:43:55 -0400

    Rebuild java template for changes to download URL

--- templates/java/generate.sh
+++ templates/java/generate.sh
@@ -9,12 +9,14 @@ function process
   image=$1
   base=$2
   version=$3
+  uuid=$4
   outpath="$OUTDIR/$image/$base/$version"
   mkdir -p "$outpath"
   sed \
       -e "s|BASE_IMAGE|$image|" \
       -e "s|BASE_FROM_VERSION|$base|" \
       -e "s|APP_VERSION|$version|" \
+      -e "s|APP_JDK_UUID|$uuid|" \
       Dockerfile > "$outpath/Dockerfile"
 }
 
@@ -26,8 +28,11 @@ VERSIONS=" \
   8u121b13 \
 "
 
+# this changes on each release
+JDK_UUID="e9e7ea248e2c4826b92b3f075a80e441"
+
 for b in $BASES; do
   for v in $VERSIONS; do
-    process $BASE_IMAGE $b $v
+    process $BASE_IMAGE $b $v $JDK_UUID
   done
 done
