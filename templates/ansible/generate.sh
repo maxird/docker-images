c15c99193995b0d8ff29df01da4b61d000c6ebac
ryan <ryanvaniderstine@maximus.com>
2017-03-25 11:55:18 -0300

    Switch to unique versions of ansible

--- templates/ansible/generate.sh
+++ templates/ansible/generate.sh
@@ -8,18 +8,28 @@ function process
 {
   image=$1
   base=$2
-  outpath="$OUTDIR/$image/$base"
+  version=$3
+  outpath="$OUTDIR/$image/$base/$version"
   mkdir -p "$outpath"
   sed \
       -e "s|BASE_IMAGE|$image|" \
       -e "s|BASE_FROM_VERSION|$base|" \
+      -e "s|APP_VERSION|$version|" \
       Dockerfile > "$outpath/Dockerfile"
 }
 
 BASE_IMAGE="centos"
 
-BASES="7"
+BASES="6 7"
+VERSIONS="
+  2.0.1.0
+  2.0.2.0
+  2.1.4.0
+  2.2.1.0
+"
 
 for b in $BASES; do
-  process $BASE_IMAGE $b
+  for v in $VERSIONS; do
+    process $BASE_IMAGE $b $v
+  done
 done
