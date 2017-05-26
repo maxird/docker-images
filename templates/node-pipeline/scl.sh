4d4331c169b4b73cadd8975335347dbdb995be63
ryan <ryanvaniderstine@maximus.com>
2017-05-26 15:13:47 -0300

    Cleanup

--- templates/node-pipeline/scl.sh
+++ templates/node-pipeline/scl.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-echo "$#:[$@]"
-
 cat > /app/_generated.sh <<EOF
 $1 $2 "$3"
 EOF
 
 chmod +x /app/_generated.sh
-
-cat /app/_generated.sh
-
 echo 'entering scl devtoolset-3...'
 scl enable devtoolset-3 /app/_generated.sh
