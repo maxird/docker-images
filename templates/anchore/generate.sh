1f688660a156ca9c4148c7e048eafc41bb91af63
ryan <ryanvaniderstine@maximus.com>
2017-08-07 09:59:47 -0300

    Adding anchore image

---
+++ templates/anchore/generate.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+SRCDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+HEREDIR=`basename $SRCDIR`
+OUTDIR="$SRCDIR/../../images/$HEREDIR"
+
+function process
+{
+  image=$1
+  base=$2
+  version=$3
+  outpath="$OUTDIR/$image/$base/$version"
+  mkdir -p "$outpath"
+  sed \
+      -e "s|BASE_IMAGE|$image|" \
+      -e "s|BASE_FROM_VERSION|$base|" \
+      Dockerfile > "$outpath/Dockerfile"
+}
+
+BASE_IMAGE="centos"
+
+BASE="7"
+
+process $BASE_IMAGE $BASE
