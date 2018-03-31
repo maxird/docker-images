63b187891c59a23867e8742e6384aeb2271d7e23
ryan <ryanvaniderstine@maximus.com>
2018-03-31 11:50:47 -0300

    Add support to disable additional logging

--- templates/lbdns/monitor.sh
+++ templates/lbdns/monitor.sh
@@ -3,9 +3,13 @@
 mkdir -p /var/log
 touch /var/log/haproxy.log
 
-rsyslogd -n &
-
-tail -f /var/log/haproxy.log &
+if [ -z "${NO_LOGGING}" ]; then
+  echo 'enhanced logging is enabled'
+  rsyslogd -n &
+  tail -f /var/log/haproxy.log &
+else
+  echo 'enhanced logging is disabled'
+fi
 
 ./parse.sh
 result=$?
