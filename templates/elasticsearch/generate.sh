70bcb042ec35f3b93d8e81816e886e7f5650a377
ryan <ryanvaniderstine@maximus.com>
2018-11-22 19:21:52 -0400

    Add ES image with discovery methods

---
+++ templates/elasticsearch/generate.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+SRCDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+HEREDIR=`basename $SRCDIR`
+OUTDIR="$SRCDIR/../../images/$HEREDIR"
+
+function process
+{
+  image=$1
+  base=$2
+  version=$3
+  outpath="$OUTDIR/$version"
+  mkdir -p "$outpath"
+  sed \
+      -e "s|BASE_IMAGE|$image|" \
+      -e "s|BASE_FROM_VERSION|$base|" \
+      -e "s|APP_VERSION|$version|" \
+      Dockerfile > "$outpath/Dockerfile"
+}
+
+BASE_IMAGE="docker.elastic.co/elasticsearch/elasticsearch"
+
+BASES="
+  6.4.3
+  6.5.1
+"
+
+for b in $BASES; do
+  process $BASE_IMAGE $b $b
+done
