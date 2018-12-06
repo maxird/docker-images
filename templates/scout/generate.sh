39e9280e397ce05e1434a943a74f9048453e4bda
ryan <ryanvaniderstine@maximus.com>
2018-12-06 16:20:34 -0400

    Adding ScoutSuite

---
+++ templates/scout/generate.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+SRCDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+HEREDIR=`basename $SRCDIR`
+OUTDIR="$SRCDIR/../../images/$HEREDIR"
+
+function process
+{
+  image=$1
+  base=$2
+  version=$3
+  outpath="$OUTDIR/$image/$base"
+  mkdir -p "$outpath"
+  sed \
+      -e "s|BASE_IMAGE|$image|" \
+      -e "s|BASE_FROM_VERSION|$base|" \
+      -e "s|APP_VERSION|$version|" \
+      Dockerfile > "$outpath/Dockerfile"
+}
+
+BASE_IMAGE="centos"
+
+BASES="7"
+
+for b in $BASES; do
+  process $BASE_IMAGE $b $NODE_VERSION
+done
