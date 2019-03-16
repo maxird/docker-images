18f93d8d9824117af5671864c693f261d8f64d70
ryan <ryanvaniderstine@maximus.com>
2019-03-16 09:55:03 -0300

    ES OD version

---
+++ templates/elasticsearch-od/generate.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+SRCDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+HEREDIR=`basename $SRCDIR`
+OUTDIR="$SRCDIR/../../images/$HEREDIR"
+
+function process
+{
+  image=$1
+  base=$2
+  version=$3
+  outpath="$OUTDIR/$version"
+  mkdir -p "$outpath"
+  sed \
+      -e "s|BASE_IMAGE|$image|" \
+      -e "s|BASE_FROM_VERSION|$base|" \
+      -e "s|APP_VERSION|$version|" \
+      Dockerfile > "$outpath/Dockerfile"
+}
+
+BASE_IMAGE="amazon/opendistro-for-elasticsearch"
+
+BASES="
+  0.7.0
+"
+
+for b in $BASES; do
+  process $BASE_IMAGE $b $b
+done
