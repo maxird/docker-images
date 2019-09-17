bb1a15b25a7a9eba766855ee056b73e764ae64e2
ryan <ryanvaniderstine@maximus.com>
2019-09-17 07:56:42 -0400

    Switch to Corretto

--- templates/java/generate.sh
+++ templates/java/generate.sh
@@ -9,6 +9,7 @@ function process
   image=$1
   base=$2
   version=$3
+  url=$4
 
   outpath="$OUTDIR/$image/$base/$version"
   mkdir -p "$outpath"
@@ -16,6 +17,7 @@ function process
       -e "s|BASE_IMAGE|$image|" \
       -e "s|BASE_FROM_VERSION|$base|" \
       -e "s|APP_VERSION|$version|g" \
+      -e "s|APP_URL|$url|g" \
       Dockerfile > "$outpath/Dockerfile"
 }
 
@@ -28,8 +30,15 @@ VERSIONS="
   11
 "
 
+VERSIONS="
+  1.8.0|https://d3pxv6yz143wms.cloudfront.net/8.222.10.1/java-1.8.0-amazon-corretto-devel-1.8.0_222.b10-1.x86_64.rpm
+  11|https://d3pxv6yz143wms.cloudfront.net/11.0.4.11.1/java-11-amazon-corretto-devel-11.0.4.11-1.x86_64.rpm
+"
+
 for b in $BASES; do
   for v in $VERSIONS; do
-    process $BASE_IMAGE $b $v
+    version=$(echo "$v" | awk -F '|' '{ print $1; }')
+    url=$(echo "$v" | awk -F '|' '{ print $2; }')
+    process $BASE_IMAGE $b $version $url
   done
 done
