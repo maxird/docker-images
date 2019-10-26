cca33da6862d8ed23f6b349967239a6a17b0e910
ryan <ryanvaniderstine@maximus.com>
2019-10-26 09:15:35 -0300

    Kibana & ES updates

--- templates/kibana/generate.sh
+++ templates/kibana/generate.sh
@@ -10,8 +10,7 @@ function process
   base=$2
   version=$3
   alerting=$4
-  sql=$5
-  jobs=$6
+  jobs=$5
   outpath="$OUTDIR/$version"
   mkdir -p "$outpath"
   sed \
@@ -19,24 +18,22 @@ function process
       -e "s|BASE_FROM_VERSION|$base|" \
       -e "s|APP_VERSION|$version|" \
       -e "s|ALERTING_VERSION|$alerting|g" \
-      -e "s|SQL_VERSION|$sql|g" \
       -e "s|JOBS_VERSION|$jobs|g" \
       Dockerfile > "$outpath/Dockerfile"
 }
 
-BASE_IMAGE="docker.elastic.co/elasticsearch/elasticsearch"
+BASE_IMAGE="docker.elastic.co/kibana/kibana"
 
-## es:alerting:sql:jobs
+## es:alerting:jobs
 #
 BASES="
   6.8.1
-  7.2.0:1.2.0.0:1.2.0.0:1.2.0
+  7.2.0:1.2.0.0:1.2.0
 "
 
 for b in $BASES; do
   version=$(echo "$b" | awk -F ':' '{ print $1; }')
   alerting=$(echo "$b" | awk -F ':' '{ print $2; }')
-  sql=$(echo "$b" | awk -F ':' '{ print $3; }')
-  jobs=$(echo "$b" | awk -F ':' '{ print $4; }')
-  process $BASE_IMAGE $version $version $alerting $sql $jobs
+  jobs=$(echo "$b" | awk -F ':' '{ print $3; }')
+  process $BASE_IMAGE $version $version $alerting $jobs
 done
